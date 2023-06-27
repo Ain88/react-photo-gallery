b1701c35bb910dea7e242b990bae5135959757cf
ahyoung in <ahyungss@hotmail.com>
2023-06-27 16:08:14 -0700

    updated

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-##React Photo Gallery
+# Sea Animal Gallery
+
+This is a gallery showcasing beautiful sea animal images. Users can search for different sea animals and view a collection of related photos.
+
+## Features
+
+- Dynamic search: Enter the name of a sea animal to see relevant images.
+- Responsive layout: The gallery adapts to different screen sizes for optimal viewing.
+- Random photo selection: Each search displays a randomized set of sea animal images.
+- Elegant font: The title uses the Playfair Display font for an elegant look.
+
+## Getting Started
+
+To run the sea animal gallery locally, follow these steps:
+
+1. Clone the repository:
+git clone https://github.com/your-username/your-repository.git
+
+2. Install dependencies:
+cd your-repository
+npm install
+
+3. Set up the environment variables:
+- Create a `.env` file in the root directory.
+- Add your Unsplash API access key to the `.env` file:
+  ```
+  REACT_APP_UNSPLASH_API_KEY=your-unsplash-api-key
+  ```
+
+4. Start the development server:
+npm start
+
+5. Open your browser and navigate to `http://localhost:3000` to view the sea animal gallery.
+
+## Dependencies
+
+The project uses the following dependencies:
+
+- React: [Link to React](https://reactjs.org/)
+- React-DOM: [Link to React-DOM](https://reactjs.org/docs/react-dom.html)
+- Axios: [Link to Axios](https://axios-http.com/)
+
+## Credits
+
+- Sea animal images provided by [Unsplash](https://unsplash.com/)
+- Icons provided by [Icons8](https://icons8.com/)
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [query, setQuery] = useState('sea animal');
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  const fetchPhotos = async () => {
+    try {
+      const response = await fetch(
+        `https://api.unsplash.com/photos/random?query=${query}&count=30&client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`
+      );
+      const data = await response.json();
+      const photoURLs = data.map((photo) => photo.urls.regular);
+      setPhotos(photoURLs);
+    } catch (error) {
+      console.log('Error fetching photos:', error);
+    }
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    fetchPhotos();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Sea Animal Gallery 🐠</h1>
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Enter a sea animal"
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div className="gallery-container">
+        {photos.map((photoURL, index) => (
+          <div key={index} className="gallery-item">
+            <img className="gallery-image" src={photoURL} alt={`Sea Animal ${index}`} />
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
